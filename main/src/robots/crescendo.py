d9d5f9ec5edf0d14247dc93b811ef220af7ff1d1
PorterMurphy <56894897+PorterMurphy@users.noreply.github.com>
2025-02-25 16:08:15 -0500

    motor IDs

--- main/src/robots/crescendo.py
+++ main/src/robots/crescendo.py
@@ -38,33 +38,33 @@ gamepad_controls = DriverControlsConfig(x_deadband=math_help.Range(0.10, 1),
                                         y_deadband=math_help.Range(0.10, 1),
                                         theta_deadband=math_help.Range(0.10, 1))
 
-shooter_config = ShooterConfig(left_motor=MotorConfig(id=11, inverted=False),
-                               right_motor=MotorConfig(id=12, inverted=True),
-                               right_flywheel_gear_ratio=1,
-                               left_flywheel_gear_ratio=1,
-                               right_flywheel_diameter_cm=12,
-                               left_flywheel_diameter_cm=12,
-                               default_velocity=1,
-                               default_fire_time=.5,
-                               default_spinup_delay=1)  # add motor configs
-indexer_config = IndexerConfig(MotorConfig(id=10, inverted=False), indexer_sensor_id=0, indexer_sensor_inverted=True,
-                               pid=PIDConfig(p=.000001, i=0, d=0, wrapping=None, tolerance=None),
-                               intake_velocity=.4, shoot_velocity=1, outtake_velocity=-1)  # fix feeder_sensor_id
-intake_config = IntakeConfig(MotorConfig(id=15, inverted=True), pid=PIDConfig(p=.000001, i=0, d=0, wrapping=None),
-                             intake_velocity=.5, outtake_velocity=-1)
+# shooter_config = ShooterConfig(left_motor=MotorConfig(id=11, inverted=False),
+#                                right_motor=MotorConfig(id=12, inverted=True),
+#                                right_flywheel_gear_ratio=1,
+#                                left_flywheel_gear_ratio=1,
+#                                right_flywheel_diameter_cm=12,
+#                                left_flywheel_diameter_cm=12,
+#                                default_velocity=1,
+#                                default_fire_time=.5,
+#                                default_spinup_delay=1)  # add motor configs
+# indexer_config = IndexerConfig(MotorConfig(id=10, inverted=False), indexer_sensor_id=0, indexer_sensor_inverted=True,
+#                                pid=PIDConfig(p=.000001, i=0, d=0, wrapping=None, tolerance=None),
+#                                intake_velocity=.4, shoot_velocity=1, outtake_velocity=-1)  # fix feeder_sensor_id
+# intake_config = IntakeConfig(MotorConfig(id=15, inverted=True), pid=PIDConfig(p=.000001, i=0, d=0, wrapping=None),
+#                              intake_velocity=.5, outtake_velocity=-1)
 climber_config = ClimberConfig(MotorConfig(id=14, inverted=False), climber_pid=PIDConfig(p=.2, i=0, d=0, wrapping=None),
                                climber_max=1)
 
-photon_camera_config = PhotonCameraConfig(camera_position=geom.Transform3d(geom.Translation3d(0, 0, 0), #camera postition on the robot xyz in meters from the center, CURRENTLY UNMEASURED
-                                                              geom.Rotation3d(0, 0, 0)), #camera rotation on the robot in degrees, CURRENTLY UNMEASURED
-                                          camera_name="set name here" #Set name from local host window
-                                          )
+# photon_camera_config = PhotonCameraConfig(camera_position=geom.Transform3d(geom.Translation3d(0, 0, 0), #camera postition on the robot xyz in meters from the center, CURRENTLY UNMEASURED
+#                                                               geom.Rotation3d(0, 0, 0)), #camera rotation on the robot in degrees, CURRENTLY UNMEASURED
+#                                           camera_name="set name here" #Set name from local host window
+#                                           )
 
-limelight_camera_config = LimelightCameraConfig(camera_position=geom.Transform3d(geom.Translation3d(0, 0, 0), #camera postition on the robot xyz in meters from the center, CURRENTLY UNMEASURED
-                                                                geom.Rotation3d(0, 0, 0)), #camera rotation on the robot in degrees, CURRENTLY UNMEASURED
-                                 camera_name=None, #Set name from nettable name if not default of 'limelight'
-                                 refresh_rate=5
-                                 )
+# limelight_camera_config = LimelightCameraConfig(camera_position=geom.Transform3d(geom.Translation3d(0, 0, 0), #camera postition on the robot xyz in meters from the center, CURRENTLY UNMEASURED
+#                                                                 geom.Rotation3d(0, 0, 0)), #camera rotation on the robot in degrees, CURRENTLY UNMEASURED
+#                                  camera_name=None, #Set name from nettable name if not default of 'limelight'
+#                                  refresh_rate=5
+#                                  )
 
 physical_properties = PhysicalConfig(wheel_diameter_cm=10.16,
                                      wheel_grip_coefficient_of_friction=1,
@@ -77,11 +77,11 @@ physical_properties = PhysicalConfig(wheel_diameter_cm=10.16,
                                      )
 
 swerve_modules = {ModulePosition.front_left:
-                      SwerveModuleConfig(drive_motor=MotorConfig(id=8, inverted=False,
+                      SwerveModuleConfig(drive_motor=MotorConfig(id=10, inverted=False,
                                                                  open_ramp_rate=swerve_ramp_rate.drive,
                                                                  closed_ramp_rate=swerve_ramp_rate.drive,
                                                                  current_limit=swerve_current_limit.drive),
-                                         angle_motor=MotorConfig(id=1, inverted=True,
+                                         angle_motor=MotorConfig(id=11, inverted=True,
                                                                  open_ramp_rate=swerve_ramp_rate.angle,
                                                                  closed_ramp_rate=swerve_ramp_rate.angle,
                                                                  current_limit=swerve_current_limit.angle),
@@ -91,11 +91,11 @@ swerve_modules = {ModulePosition.front_left:
                                          angle_pid=default_angle_pid,
                                          drive_pid=default_drive_pid),
                   ModulePosition.front_right:
-                      SwerveModuleConfig(drive_motor=MotorConfig(id=6, inverted=False,
+                      SwerveModuleConfig(drive_motor=MotorConfig(id=12, inverted=False,
                                                                  open_ramp_rate=swerve_ramp_rate.drive,
                                                                  closed_ramp_rate=swerve_ramp_rate.drive,
                                                                  current_limit=swerve_current_limit.drive),
-                                         angle_motor=MotorConfig(id=7, inverted=True,
+                                         angle_motor=MotorConfig(id=13, inverted=True,
                                                                  open_ramp_rate=swerve_ramp_rate.angle,
                                                                  closed_ramp_rate=swerve_ramp_rate.angle,
                                                                  current_limit=swerve_current_limit.angle),
@@ -105,11 +105,11 @@ swerve_modules = {ModulePosition.front_left:
                                          angle_pid=default_angle_pid,
                                          drive_pid=default_drive_pid),
                   ModulePosition.back_right:
-                      SwerveModuleConfig(drive_motor=MotorConfig(id=4, inverted=False,
+                      SwerveModuleConfig(drive_motor=MotorConfig(id=14, inverted=False,
                                                                  open_ramp_rate=swerve_ramp_rate.drive,
                                                                  closed_ramp_rate=swerve_ramp_rate.drive,
                                                                  current_limit=swerve_current_limit.drive),
-                                         angle_motor=MotorConfig(id=5, inverted=True,
+                                         angle_motor=MotorConfig(id=15, inverted=True,
                                                                  open_ramp_rate=swerve_ramp_rate.angle,
                                                                  closed_ramp_rate=swerve_ramp_rate.angle,
                                                                  current_limit=swerve_current_limit.angle),
@@ -119,11 +119,11 @@ swerve_modules = {ModulePosition.front_left:
                                          angle_pid=default_angle_pid,
                                          drive_pid=default_drive_pid),
                   ModulePosition.back_left:
-                      SwerveModuleConfig(drive_motor=MotorConfig(id=2, inverted=False,
+                      SwerveModuleConfig(drive_motor=MotorConfig(id=16, inverted=False,
                                                                  open_ramp_rate=swerve_ramp_rate.drive,
                                                                  closed_ramp_rate=swerve_ramp_rate.drive,
                                                                  current_limit=swerve_current_limit.drive),
-                                         angle_motor=MotorConfig(id=3, inverted=True,
+                                         angle_motor=MotorConfig(id=17, inverted=True,
                                                                  open_ramp_rate=swerve_ramp_rate.angle,
                                                                  closed_ramp_rate=swerve_ramp_rate.angle,
                                                                  current_limit=swerve_current_limit.angle),
